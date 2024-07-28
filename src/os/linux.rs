c0cb76001dc5e9d972e645502723e392270ac85c
Fwuff <FwuffFox@gmail.com>
2024-07-28 18:22:18 +0300

    add process_line and add timeout field to output

--- src/os/linux.rs
+++ src/os/linux.rs
@@ -6,6 +6,7 @@ pub struct EfiBootMgr;
 pub struct EfiBootMgrOutput {
     boot_next: Option<u32>,
     boot_current: u32,
+    timeout: Option<u32>,
     boot_order: Vec<u32>,
     boot_entry: Vec<EfiBootEntry>,
 }
@@ -15,6 +16,7 @@ impl EfiBootMgrOutput {
         EfiBootMgrOutput {
             boot_next: None,
             boot_current: 0,
+            timeout: None,
             boot_order: vec![],
             boot_entry: vec![],
         }
@@ -35,36 +37,45 @@ impl EfiBootMgr {
         let result = output.stdout.split(|&x| x == b'\n');
 
         for line in result {
-            let parts = line.split_once(|&x| x == b':');
-
-            match parts {
-                Some(x) => {
-                    let (name, value) = (
-                        String::from_utf8(x.0.to_vec())?,
-                        String::from_utf8(x.1.to_vec())?,
-                    );
-
-                    let (name, value) = (name.trim(), value.trim());
-
-                    match name {
-                        "BootNext" => efibootmgr_output.boot_next = Some(value.parse()?),
-
-                        "BootCurrent" => efibootmgr_output.boot_current = value.parse()?,
-
-                        "BootOrder" => {
-                            efibootmgr_output.boot_order = value
-                                .split(|x| x == ',')
-                                .map(|x| x.trim().parse().unwrap())
-                                .collect()
-                        }
-
-                        _ => println!("Unknown entry: {name} {value}"),
-                    }
-                }
-                None => println!("No boot enries support yet"),
-            }
+            Self::process_line(&mut efibootmgr_output, line)?;
         }
 
         Ok(efibootmgr_output)
+    }
+
+    fn process_line(
+        efibootmgr_output: &mut EfiBootMgrOutput,
+        line: &[u8],
+    ) -> Result<(), Box<dyn Error>> {
+        let parts = line.split_once(|&x| x == b':');
+        match parts {
+            Some(x) => {
+                let (name, value) = (
+                    String::from_utf8(x.0.to_vec())?,
+                    String::from_utf8(x.1.to_vec())?,
+                );
+
+                let (name, value) = (name.trim(), value.trim());
+
+                match name {
+                    "BootNext" => efibootmgr_output.boot_next = Some(value.parse()?),
+
+                    "BootCurrent" => efibootmgr_output.boot_current = value.parse()?,
+
+                    "BootOrder" => {
+                        efibootmgr_output.boot_order = value
+                            .split(|x| x == ',')
+                            .map(|x| x.trim().parse().unwrap())
+                            .collect()
+                    },
+
+                    "Timeout" => efibootmgr_output.timeout = Some(value.parse()?),
+
+                    _ => println!("Unknown entry: {name} {value}"),
+                }
+            }
+            None => println!("No boot enries support yet"),
+        }
+        Ok(())
     }
 }
